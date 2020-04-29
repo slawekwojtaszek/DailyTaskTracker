4c8709124ededaabd7ea62e25b8c5fb121687d7b
Slawek Wojtaszek <wojtaszek@protonmail.com>
2020-04-29 21:12:18 +0100

    Add filter task feature

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const form = document.querySelector("form");
 const input = document.querySelector("#new-task");
 const listOfTasks = document.querySelector(".tasks");
 const deleteBtn = document.querySelector(".delete");
+const filter = document.querySelector(".filter");
 
 function addTask(e) {
   let newTask = input.value;
@@ -37,6 +38,23 @@ function markTask(e) {
   }
 }
 
+function handleFilter(e) {
+  const input = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".single-task").forEach(function (task) {
+    let item = task.firstChild.textContent;
+
+    if (item.toLowerCase().indexOf(input) !== -1) {
+      task.style.display = "flex";
+      console.log("siema");
+    } else {
+      task.style.display = "none";
+      console.log("nie-siema");
+    }
+  });
+}
+
 form.addEventListener("submit", addTask);
 listOfTasks.addEventListener("click", markTask);
 listOfTasks.addEventListener("click", removeTask);
+filter.addEventListener("input", handleFilter);
