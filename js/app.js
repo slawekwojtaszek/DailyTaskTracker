e9081412f9e28b21bff8c3a6d865e5fe1b12e876
Slawek Wojtaszek <wojtaszek@protonmail.com>
2021-06-07 21:37:24 +0100

    CSS Refactoring

--- js/app.js
+++ js/app.js
@@ -7,52 +7,55 @@ const filter = document.querySelector(".filter");
 const tasksNumber = document.querySelector(".number-tasks");
 
 function addTask(e) {
-  let newTask = input.value;
-  if (newTask !== "") {
-    const li = document.createElement("li");
-    // const h1 = document.createElement("h1");
-    li.className = "single-task";
-    li.appendChild(document.createTextNode(newTask));
-    // li.appendChild(h1);
-    const span = document.createElement("span");
-    span.className = "delete";
-    span.appendChild(document.createTextNode("X"));
-    li.appendChild(span);
-    listOfTasks.appendChild(li);
-  }
+   let newTask = input.value;
+   if (newTask !== "") {
+      const li = document.createElement("li");
+      const p = document.createElement("p");
+      // const h1 = document.createElement("h1");
 
-  input.value = "";
-  e.preventDefault();
+      li.className = "single-task";
+
+      li.appendChild(document.createTextNode(newTask));
+      // li.appendChild(h1);
+      const span = document.createElement("span");
+      span.className = "delete";
+      span.appendChild(document.createTextNode("X"));
+      li.appendChild(span);
+      listOfTasks.appendChild(li);
+   }
+
+   input.value = "";
+   e.preventDefault();
 }
 
 function removeTask(e) {
-  if (e.target.parentElement.classList.contains("single-task")) {
-    console.log("yes");
+   if (e.target.parentElement.classList.contains("single-task")) {
+      console.log("yes");
 
-    e.target.parentElement.remove();
-  }
+      e.target.parentElement.remove();
+   }
 }
 
 function markTask(e) {
-  if (e.target.parentElement.classList.value === "tasks") {
-    e.target.classList.toggle("done");
-  }
+   if (e.target.parentElement.classList.value === "tasks") {
+      e.target.classList.toggle("done");
+   }
 }
 
 function handleFilter(e) {
-  const input = e.target.value.toLowerCase();
+   const input = e.target.value.toLowerCase();
 
-  document.querySelectorAll(".single-task").forEach(function (task) {
-    let item = task.firstChild.textContent;
+   document.querySelectorAll(".single-task").forEach(function (task) {
+      let item = task.firstChild.textContent;
 
-    if (item.toLowerCase().indexOf(input) !== -1) {
-      task.style.display = "flex";
-      console.log("siema");
-    } else {
-      task.style.display = "none";
-      console.log("nie-siema");
-    }
-  });
+      if (item.toLowerCase().indexOf(input) !== -1) {
+         task.style.display = "flex";
+         console.log("siema");
+      } else {
+         task.style.display = "none";
+         console.log("nie-siema");
+      }
+   });
 }
 
 form.addEventListener("submit", addTask);
