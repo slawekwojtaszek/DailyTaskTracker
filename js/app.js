a91e61b180fc4e0a09361a4996519fc98183ec75
Slawek Wojtaszek <wojtaszek@protonmail.com>
2022-03-02 22:42:46 +0000

    Fix double rendering issue

--- js/app.js
+++ js/app.js
@@ -6,100 +6,95 @@ const deleteBtn = document.querySelector(".delete");
 const filter = document.querySelector(".filter");
 const tasksNumber = document.querySelector(".number-tasks");
 const dateElement = document.querySelector(".date");
-const test = document.querySelector(".test");
-const number = document.querySelector(".number");
 
-const myArray = [];
-const myArray2 = [];
+// const myDate = new Date();
+// const months = [
+//    "January",
+//    "February",
+//    "March",
+//    "April",
+//    "May",
+//    "June",
+//    "July",
+//    "August",
+//    "September",
+//    "October",
+//    "November",
+//    "December",
+// ];
+// const days = [
+//    "Sunday",
+//    "Monday",
+//    "Tuesday",
+//    "Wednesday",
+//    "Thursday",
+//    "Friday",
+//    "Saturday",
+// ];
 
-const myDate = new Date();
-const months = [
-   "January",
-   "February",
-   "March",
-   "April",
-   "May",
-   "June",
-   "July",
-   "August",
-   "September",
-   "October",
-   "November",
-   "December",
-];
-const days = [
-   "Sunday",
-   "Monday",
-   "Tuesday",
-   "Wednesday",
-   "Thursday",
-   "Friday",
-   "Saturday",
-];
+// const day = myDate.getDay();
+// const month = myDate.getMonth();
+// const year = myDate.getFullYear();
+// const todaysDate = `${days[day]}, ${day} ${months[month]} ${year}`;
 
-const day = myDate.getDay();
-const month = myDate.getMonth();
+// dateElement.textContent = todaysDate;
 
-let buttonOn = false;
+let data = ["list1", "list2", "list3"];
+let total = ["list1", "list2", "list3"];
 
-const year = myDate.getFullYear();
-
-const todaysDate = `${days[day]}, ${day} ${months[month]} ${year}`;
-
-dateElement.textContent = todaysDate;
-
-function addFromArray() {
-   const li = document.createElement("li");
-   const p = document.createElement("p");
-
-   myArray.map((item) => {
-      li.appendChild(document.createTextNode(item));
-      console.log(li);
-      li.className = "single-task";
-      listOfTasks.appendChild(li);
-      const span = document.createElement("span");
-      span.className = "delete";
-      span.appendChild(document.createTextNode("X"));
-      li.appendChild(span);
-      number.textContent = myArray2.length + 1;
-   });
-
-   const newArray = myArray.pop();
-   myArray2.push(newArray);
-
-   const newArrayLen = myArray2.length;
-
-   console.log(myArray2);
-   console.log(newArrayLen);
-}
+let flag = false;
 
 function addTask(e) {
    let newTask = input.value;
-   if (newTask !== "") {
+   data.push(newTask);
+   total.push(newTask);
+   // if (newTask !== "") {
+   //    data.push(newTask);
+   //    const li = document.createElement("li");
+   //    const p = document.createElement("p");
+
+   //    li.className = "single-task";
+   //    li.appendChild(document.createTextNode(newTask));
+   //    const span = document.createElement("span");
+   //    span.className = "delete";
+   //    span.appendChild(document.createTextNode("X"));
+   //    li.appendChild(span);
+   //    listOfTasks.prepend(li);
+   // }
+
+   input.value = "";
+
+   displayTask();
+
+   e.preventDefault();
+}
+displayTask();
+
+function displayTask() {
+   const li = document.createElement("li");
+   const p = document.createElement("p");
+   for (let i = 0; i < data.length; i++) {
       const li = document.createElement("li");
-      const p = document.createElement("p");
-      // const h1 = document.createElement("h1");
-
+      li.appendChild(document.createTextNode(data[i]));
       li.className = "single-task";
-
-      li.appendChild(document.createTextNode(newTask));
-      // li.appendChild(h1);
       const span = document.createElement("span");
       span.className = "delete";
       span.appendChild(document.createTextNode("X"));
-      li.appendChild(span);
-      listOfTasks.appendChild(li);
-      myArray.push(newTask);
-      addFromArray();
+      li.append(span);
+      console.log(total.length);
+      listOfTasks.prepend(li);
    }
+   data = [];
 
-   input.value = "";
-   e.preventDefault();
+   // const p = document.createElement("p");
+   // li.className = "single-task";
+   // const span = document.createElement("span");
+   // span.className = "delete";
+   // span.appendChild(document.createTextNode("X"));
+   // li.appendChild(span);
 }
 
 function removeTask(e) {
-   // const el = e.target.parentElement;
-
    if (e.target.parentElement.classList.contains("single-task")) {
       e.target.parentElement.remove();
    }
@@ -133,19 +128,7 @@ function handleFilter(e) {
    });
 }
 
-function handleOpeningFilter(e) {
-   e.preventDefault();
-   if (!buttonOn) {
-      filter.classList.add("tak");
-      buttonOn = true;
-   } else {
-      filter.classList.remove("tak");
-      buttonOn = false;
-   }
-}
-
 form.addEventListener("submit", addTask);
 listOfTasks.addEventListener("click", markTask);
 listOfTasks.addEventListener("click", removeTask);
 filter.addEventListener("input", handleFilter);
-test.addEventListener("click", handleOpeningFilter);
